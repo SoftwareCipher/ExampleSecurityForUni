7243aa258e8704547523592f2214df07f19e2ea1
Observer <kirillkuznetsov181@gmail.com>
2023-02-19 17:53:03 +0200

    project showing work with Http requests and authorization for uni.

---
+++ src/main/java/com/english/service/app/TrustMeEnglishApplication.java
@@ -0,0 +1,12 @@
+package com.english.service.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustMeEnglishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrustMeEnglishApplication.class, args);
+    }
+}
---
+++ src/test/java/com/english/service/app/TrustMeEnglishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.english.service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustMeEnglishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
